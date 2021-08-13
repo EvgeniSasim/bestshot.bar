fc272cc453d0af4a24908271b6717c5e759d72d1
EvgeniSasim <86315692+EvgeniSasim@users.noreply.github.com>
2021-08-13 14:37:31 +0300

    show work

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+console.log('Example')
+;
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+function eng() {
+    document.getElementById("ru").style.display = "none";
+}
